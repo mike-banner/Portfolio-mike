9cd1faa2c42f523f22dbaef226aac03193330f1e
mike_banner <mike.webfree@gmail.com>
2026-05-27 16:36:38 +0200

    feat: complete structure of landing page and fix sitemap compatibility for Astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://portfolio-mike.pages.dev', // Remplacer par ton domaine personnalisé
+  integrations: [sitemap()]
+});
